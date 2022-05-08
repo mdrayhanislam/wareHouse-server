a8ba421799e68608bdeb4596c9cf849fd066625b
mdrayhanislam <96991135+mdrayhanislam@users.noreply.github.com>
2022-05-08 17:45:03 +0600

    ObjectId import

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const { listen } = require('express/lib/application');
 require('dotenv').config();
@@ -29,10 +29,10 @@ async function run(){
         const services = await cursor.toArray();
         res.send(services);
     });
-    app.get('/service', async (req, res) => {
+    app.get('/service/:id', async (req, res) => {
         const id = req.params.id;
         const query ={_id: ObjectId(id)};
-        const service =await serviceCollection.findOne(query)
+        const service = await serviceCollection.findOne(query);
         res.send(service);
     })
 
