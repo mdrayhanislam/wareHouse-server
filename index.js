c910ebd12246cb2f680240deb6ea2e34d5426fac
mdrayhanislam <96991135+mdrayhanislam@users.noreply.github.com>
2022-05-08 17:17:12 +0600

    service api create

--- index.js
+++ index.js
@@ -27,7 +27,15 @@ async function run(){
         const query = {};
         const cursor = serviceCollection.find(query);
         const services = await cursor.toArray();
+        res.send(services);
+    });
+    app.get('/service', async (req, res) => {
+        const id = req.params.id;
+        const query ={_id: ObjectId(id)};
+        const service =await serviceCollection.findOne(query)
+        res.send(service);
     })
+
     }
     finally{
 
