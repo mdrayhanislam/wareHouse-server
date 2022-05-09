40bb3cd980908fb8c79614eb06bc9b363f4239f2
mdrayhanislam <96991135+mdrayhanislam@users.noreply.github.com>
2022-05-09 22:16:45 +0600

    was problem this problem solve

--- index.js
+++ index.js
@@ -36,6 +36,22 @@ async function run(){
         res.send(service);
     })
 
+     //POST
+     app.post('/service', async(req, res) => {
+        const newService = req.body;
+        const result = await serviceCollection.insertOne(newService);
+        res.send(result);
+    })
+
+    //DELETE
+    app.delete('/service/:id', async(req, res) => {
+        const id = req.params.id;
+        const query ={_id: ObjectId(id)};
+        const result = await serviceCollection.deleteOne(query);
+        res.send(result);
+    });
+
+    
     }
     finally{
 
